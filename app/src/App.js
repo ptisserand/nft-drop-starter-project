93838f2858e0fd63514c66b25a1b7926c532d423
Patrice Tisserand <p.tisserand@gmail.com>
2021-12-07 23:21:07 +0100

    Added 'connect wallet' button

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -8,13 +8,22 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
 
+  const [walletAddress, setWalletAddress] = useState(null);
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
       if (solana) {
         if (solana.isPhantom) {
           console.log("Phantom wallet found!");
+
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log('Connected with public key', response.publicKey.toString());
+
+          /* set user's publickey in state */
+          setWalletAddress(response.publicKey.toString());
         }
+
       } else {
         alert('Solana object not found! Get a Phantom Wallet');
       }
@@ -22,6 +31,29 @@ const App = () => {
       console.error(error);
     }
   }
+
+  const connectWallet = async () => { 
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with public key', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  /*
+ * We want to render this UI when the user hasn't connected
+ * their wallet to our app yet.
+ */
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
 
   /* when component first mount */
   useEffect(() => {
@@ -38,6 +70,8 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {/* To connect wallet */}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
